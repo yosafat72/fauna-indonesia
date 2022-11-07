45acbabb609ce0812ccce94ca274298cbd3a1f67
Yosafat <yosafat.suryana@mncbank.co.id>
2022-11-07 10:17:11 +0700

    first commit

---
+++ app/src/main/java/lab/yohesu/faunaindonesia/MainActivity.kt
@@ -0,0 +1,11 @@
+package lab.yohesu.faunaindonesia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
