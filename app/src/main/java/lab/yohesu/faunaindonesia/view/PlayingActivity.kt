f00a564538312fdbd510f9ed071aede8d3106618
Yosafat <yosafat.suryana@mncbank.co.id>
2022-11-13 15:12:22 +0700

    add view playing with one data

--- app/src/main/java/lab/yohesu/faunaindonesia/view/PlayingActivity.kt
+++ app/src/main/java/lab/yohesu/faunaindonesia/view/PlayingActivity.kt
@@ -7,9 +7,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import lab.yohesu.faunaindonesia.databinding.ActivityPlayingBinding
+import lab.yohesu.faunaindonesia.model.PlayingDataModel
 import lab.yohesu.faunaindonesia.model.PlayingModel
 import lab.yohesu.faunaindonesia.service.Status
 import lab.yohesu.faunaindonesia.viewmodel.PlayingViewModel
@@ -49,11 +51,24 @@ class PlayingActivity : AppCompatActivity() {
 
     private fun onSuccess(model: PlayingModel?) {
         Log.d("RESULT", model.toString())
+        if (model != null) {
+            model.data?.first()?.let { setToView(model = it) }
+        }
     }
 
     private fun onError(message: String?) {
         Log.d("Running-Error", "$message")
         finish()
+    }
+
+    private fun setToView(model: PlayingDataModel){
+        model.question.let { binding.txtQuestion.text = it }
+        model.optionA.let { binding.radioOptionA.text = it }
+        model.optionB.let { binding.radioOptionB.text = it }
+        model.optionC.let { binding.radioOptionC.text = it }
+        model.optionD.let { binding.radioOptionD.text = it }
+        model.imageQuestion.let { Glide.with(this).load(it).fitCenter().into(binding.imgQuestion) }
+
     }
 
 }
